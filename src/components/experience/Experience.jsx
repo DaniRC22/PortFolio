602e18365eb9d5602aad44ac7ec6becf56c27897
Esteban Daniel Rivero <100779672+DaniRC22@users.noreply.github.com>
2023-07-10 17:45:08 -0300

    Arreglo de sintaxis

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -43,7 +43,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
-              <h4>Angukar</h4>
+              <h4>Angular</h4>
               <small className='text-light'>intermedio</small>
               </div>
             </article>
